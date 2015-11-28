2b5a7020069d18fc9603d41c633e2f119f278d94
Isaac W Hanson <isaac@starlig.ht>
2015-11-28 12:53:15 -0500

    start functions with _ in keychain

--- etc/profile.d/keychain.sh
+++ etc/profile.d/keychain.sh
@@ -4,15 +4,15 @@ if [ -z "$DESKTOP_SESSION" ] && [ "$SHLVL" -eq "1" ]; then
 
     MINTTY_COUNT_FILE="$HOME/.keychain/mintty_count"
 
-function start_agent {
+function _start_agent {
     if [ ! -e "$MINTTY_COUNT_FILE" ]; then
         echo 'export MINTTY_COUNT=1' > "$MINTTY_COUNT_FILE"
         chmod 600 "${MINTTY_COUNT_FILE}"
     fi
-    eval `keychain -q --eval --ignore-missing --agents ssh id_rsa`
+    eval `keychain -q -Q --eval --ignore-missing --agents ssh id_rsa`
 }
 
-function stop_agent {
+function _stop_agent {
     . "$MINTTY_COUNT_FILE"
     if [ "$MINTTY_COUNT" -le 1 ]; then
 	rm -f $MINTTY_COUNT_FILE
@@ -27,7 +27,7 @@ function stop_agent {
         . "${MINTTY_COUNT_FILE}" 2>/dev/null
         echo "export MINTTY_COUNT=$((MINTTY_COUNT+1))" > "$MINTTY_COUNT_FILE"
         unset MINTTY_COUNT
-        start_agent;
-        trap stop_agent 0
+        _start_agent;
+        trap _stop_agent 0
     fi
 fi
